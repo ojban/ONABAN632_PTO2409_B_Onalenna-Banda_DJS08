248e5a7c1eeb24b4d66c6a2fe1c2b120b49b0d7c
Onalenna Banda <onabanda@gmail.com>
2025-06-15 23:07:45 +0200

    avd react

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,69 @@
-/**
- * Challenge:
- * Bootstrap the VanLife project by creating the first 2 routes:
- * Home and About.
- * 
- * Also include the navbar that can link between the two routes.
- * For now, you'll either need to copy/paste the navbar code
- * to both Home and About pages, or you'll need to find a place
- * to put it where it can be shared between the two pages.
- * (Don't overthink this part - just do whatever is easiest for
- * you because we'll learn a better approach very soon)
- * 
- * Review challenge: do all the CSS yourself based on the design
- * linked in the slides.
- */
+// Import necessary modules from React and React Router
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
+// PAGE COMPONENT IMPORTS
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Vans from "./pages/Vans";
+import VanDetail from "./pages/VanDetail";
+
+// impport layout components
+import Layout from "./components/Layout";
+import HostLayout from "./components/HostLayout";
+
+// import host page components
+import Dashboard from "./pages/Host/Dashboard";
+import Income from "./pages/Host/Income";
+import Reviews from "./pages/Host/Reviews";
+import HostVans from "./pages/Host/HostVans";
+import HostVanDetail from "./pages/Host/HostVanDetail";
+import HostVanInfo from "./pages/Host/HostVanInfo";
+import HostVanPricing from "./pages/Host/HostVanPricing";
+import HostVanPhotos from "./pages/Host/HostVanPhotos";
+
+// Import the server configuration
+import "./server";
+
+// Define the main application component
 function App() {
-
   return (
-    <h1>Start here</h1>
-  )
+    // Set up the BrowserRouter for routing
+    <BrowserRouter>
+      {/* Define the route structure */}
+      <Routes>
+        {/* Main layout route */}
+        <Route path="/" element={<Layout />}>
+          {/* Home page route */}
+          <Route index element={<Home />} />
+          {/* About page route */}
+          <Route path="about" element={<About />} />
+          {/* Vans listing page route */}
+          <Route path="vans" element={<Vans />} />
+          {/* Van detail page route with dynamic parameter */}
+          <Route path="vans/:id" element={<VanDetail />} />
+
+          {/* Host layout route */}
+          <Route path="host" element={<HostLayout />}>
+            {/* Host dashboard route */}
+            <Route index element={<Dashboard />} />
+            {/* Host income page route */}
+            <Route path="income" element={<Income />} />
+            {/* Host reviews page route */}
+            <Route path="reviews" element={<Reviews />} />
+            {/* Host vans listing page route */}
+            <Route path="vans" element={<HostVans />} />
+            {/* Host van detail page route with nested routes for info, pricing, and photos */}
+            <Route path="vans/:id" element={<HostVanDetail />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
+            </Route>
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+// Export the App component as the default export
+export default App;
